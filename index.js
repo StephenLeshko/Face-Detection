d12591976f63fc7556863cd470a73350868c8333
StephenLeshko <stephen.leshko@gmail.com>
2023-04-11 19:32:40 -0400

    loading

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const video = document.getElementById('video')
+
+Promise.all(
+    [
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    ]   
+    )
+
+function startVideo(){
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+
+
+}
+
