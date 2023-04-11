9e467b9028ecda02e12c99ce17d83d9ec0ef18ce
StephenLeshko <stephen.leshko@gmail.com>
2023-04-11 19:42:29 -0400

    detections

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ Promise.all(
         faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
         faceapi.nets.faceExpressionNet.loadFromUri('/models'),
     ]   
-    )
+    ).then(startVideo)
 
 function startVideo(){
     navigator.getUserMedia(
@@ -15,7 +15,26 @@ function startVideo(){
         stream => video.srcObject = stream,
         err => console.error(err)
     )
-
-
 }
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+    
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions()
+        
+
+
+    })
+
+
+
+
+})
+
 
